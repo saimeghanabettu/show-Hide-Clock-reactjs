175134b327d060191daaa32c5661d716c37b38f8
Sai Meghana Bettu <saimeghanabettu@gmail.com>
2023-05-26 12:12:30 +0000

    show-hide-clockapp-using-componentlifecycle

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import {Component} from 'react'
+import Clock from './components/Clock'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    showClock: false,
+  }
+
+  onToggleClick = () => {
+    this.setState(prevState => {
+      const {showClock} = prevState
+      return {
+        showClock: !showClock,
+      }
+    })
+  }
+
+  render() {
+    const {showClock} = this.state
+    return (
+      <div className="app-container">
+        <button
+          type="button"
+          className="toggle-button"
+          onClick={this.onToggleClick}
+        >
+          {showClock ? 'Hide Clock' : 'Show Clock'}
+        </button>
+        {showClock && <Clock />}
+      </div>
+    )
+  }
+}
+
+export default App
